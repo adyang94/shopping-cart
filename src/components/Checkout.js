0bb14417bf0447e2bc986972f151796ecb7e6c92
adyang94 <adyang94@yahoo.com>
2021-05-15 13:47:21 -0500

    adding checkout outline

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,53 +2,62 @@ import React from 'react';
 
 function Checkout() {
   return (
-    <div>
+    <div className="checkoutCtn">
       <form>
-        <input placeholder="Email" />
+        <input placeholder="Email" style={{ width: '75%' }} />
         <br />
-        <input type="checkbox" />
-        Add email for the latest updates and deals!
+        <div>
+          <input type="checkbox" />
+          Add email for the latest updates and deals!
+        </div>
 
-        <h3>Shipping Info</h3>
-        <input placeholder="First Name" />
-        <input placeholder="Last Name" />
-        <br />
+        <div className="shippingInfoCtn">
+          <h3>Shipping Info</h3>
+          <div>
+            <input placeholder="First Name" />
+            <input placeholder="Last Name" />
+            <br />
+          </div>
 
-        <input placeholder="Company (optional)" />
-        <br />
-        <input placeholder="Address (include apartment number if needed" />
-        <br />
-        <input placeholder="City" />
-        <br />
-        <input placeholder="State" />
-        <br />
-        <input placeholder="Country" />
-        <br />
-        <input placeholder="Phone (optional)" />
-        <br />
+          <input placeholder="Company (optional)" />
+          <br />
+          <input placeholder="Address (include apartment number if needed" />
+          <br />
+          <input placeholder="City" />
+          <br />
+          <input placeholder="State" />
+          <br />
+          <input placeholder="Country" />
+          <br />
+          <input placeholder="Phone (optional)" />
+          <br />
+        </div>
 
-        <h3>Payment</h3>
-        <br />
-        <input placeholder="Credit Card Number" />
-        <br />
-        <input placeholder="Expiration Date" />
-        <input placeholder="CVC (3 digits on back of card)" />
-        <br />
-        <input placeholder="First Name" />
-        <input placeholder="Last Name" />
-        <br />
-        <input placeholder="Company (optional)" />
-        <br />
-        <input placeholder="Address (include apartment number if needed)" />
-        <br />
-        <input placeholder="City" />
-        <br />
-        <input placeholder="State" />
-        <br />
-        <input placeholder="Country" />
-        <br />
-        <input placeholder="Phone (optional)" />
+        <div className="paymentCtn">
+          <h3>Payment</h3>
+          <input placeholder="Credit Card Number" />
+          <br />
+          <input placeholder="Expiration Date" />
+          <input placeholder="CVC (3 digits on back of card)" />
+          <br />
+          <input placeholder="First Name" />
+          <input placeholder="Last Name" />
+          <br />
+          <input placeholder="Company (optional)" />
+          <br />
+          <input placeholder="Address (include apartment number if needed)" />
+          <br />
+          <input placeholder="City" />
+          <br />
+          <input placeholder="State" />
+          <br />
+          <input placeholder="Country" />
+          <br />
+          <input placeholder="Phone (optional)" />
+          <br />
+        </div>
 
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
